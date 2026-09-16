a6f3571f7aa3095d21553642c4380d056942acf1
Abhishek8841 <abhishekbatra2005@gmail.com>
2026-09-16 11:08:25 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/abhishek8841/go-monolith/internal/config"
+)
 
 func main() {
-	fmt.Println("Backend is up and running..")
+	cfg := config.MustLoad()
+
+	fmt.Println("Starting the server now...")
+
+	// if we did http.HandleFunc then automatically the global mux would be registered which is considered bad practice so we create new mux and use that...
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		// here order matters ie write header flushes the header and we cant add new headers after that for .header.set method which just registers the headers should be written first
+
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	srv := http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	// mux is also a type of handler that can store a lot of other handlers
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed : %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abhishek8841/go-monolith
 
 go 1.26.6
+
+require github.com/joho/godotenv v1.5.1
